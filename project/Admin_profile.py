d8e435e116738a2154e0d80053ccf9453aa6dbd1
Mihael Jigov <pplesho@gmail.com>
2023-12-15 16:37:23 +0200

    Not filed const

--- project/Admin_profile.py
+++ project/Admin_profile.py
@@ -2,9 +2,12 @@ from project.Profile import Profile
 
 
 class AdminProfile(Profile):
-    def __init__(self, username, age, gender, email, password, bio, contact_information, location, education, hobbies):
-        super().__init__(username, age, gender, email, password, bio, contact_information, location, education, hobbies,
-                         False, False, False, False)
+    NOT_FILLED = 'Not filed'
+
+    def __init__(self, username, age, email, password, gender=NOT_FILLED, bio=NOT_FILLED,
+                 contact_information=NOT_FILLED, location=NOT_FILLED, education=NOT_FILLED, hobbies=NOT_FILLED):
+        super().__init__(username, age, email, password, gender, bio, contact_information, location, education, hobbies,
+                         False, True, False, False)
 
     def profile_permissions(self):
         pass
