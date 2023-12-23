a197eb2ba084514486df8ffde9467edf573c9286
Mihael Jigov <pplesho@gmail.com>
2023-12-23 20:54:05 +0200

    fix bugs and add displaying functions

--- project/Main_functions/Post.py
+++ project/Main_functions/Post.py
@@ -32,19 +32,46 @@ class Post(Validator):
         self.likes_list.append(user)
 
     def comment_post(self, user, comment):
-        self.comments_list.append(f"{user}: {comment}")
+        self.comments_list.append(f"{user.username}: {comment}")
 
     def __repr__(self):
         return f"{self.user.username.capitalize()}'s post"
 
-    def __str__(self):
-        result = ['===============================',
+    def display_likes(self):
+        info = ['===============================',
                   f"Post by: {self.user.username.capitalize()}",
                   f"-------------------------------",
                   f"{self.picture}",
                   f"-------------------------------",
                   f"Text: {self.text}",
-                  f"Likes: {self.likes_count} Comments: {self.comment_count}",
-                  '===============================',
-                  ]
-        return '\n'.join(result)
+                  f"Comments: {self.comment_count}",
+                  f"VVV Likes:{self.likes_count} VVV"]
+        list_of_users = [f"{user.username}" for user in self.likes_list]
+
+        return '\n'.join(info+list_of_users)
+
+    def display_comments(self):
+        info = ['===============================',
+                  f"Post by: {self.user.username.capitalize()}",
+                  f"-------------------------------",
+                  f"{self.picture}",
+                  f"-------------------------------",
+                  f"Text: {self.text}",
+                  f"Likes:{self.likes_count}",
+                  f"VVV Comments: {self.comment_count} VVV"]
+        list_of_comments = [comment for comment in self.comments_list]
+
+        return '\n'.join(info+list_of_comments)
+
+
+def __str__(self):
+    result = ['===============================',
+              f"Post by: {self.user.username.capitalize()}",
+              f"-------------------------------",
+              f"{self.picture}",
+              f"-------------------------------",
+              f"Text: {self.text}",
+              f"Likes: {self.likes_count} Comments: {self.comment_count}",
+              '===============================',
+              ]
+    return '\n'.join(result)
