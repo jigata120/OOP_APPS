ade878b3ac35b95c4635d90b08330b4ddcb52096
Mihael Jigov <pplesho@gmail.com>
2023-12-27 12:33:04 +0200

    options created

--- project/Main_App/Social_media.py
+++ project/Main_App/Social_media.py
@@ -1,17 +1,18 @@
-from project.Main_App.Events import SocialMediaFunctionalities
+from project.Main_App.Events import SocialMediaFunctionalities, user1
 
 
 class SocialMedia(SocialMediaFunctionalities):
 
     def __init__(self):
         super().__init__()
-        self.user = super().login()
+        self.user = user1  # super().login()
+
         self.main_events = {
             "content": self.content,
-            "create post": self.create_post,
-            "create chat": self.create_chat,
-            "search user": self.search_name,
-            "search post": self.search_post,
+            "create_post": self.create_post,
+            "create_chat": self.create_chat,
+            "search_user": self.search_name,
+            "search_post": self.search_post,
             "logout": self.logout
         }
         self.main_options()
@@ -29,20 +30,50 @@ class SocialMedia(SocialMediaFunctionalities):
 
     def content(self):
         content = super().personal_content(self.user)
-        return str(content)
+        return content
 
     def search_name(self):
         # ask for username
         username = input("Search by username: ")
         user = super().find_user(username)
+        return user
 
-    def search_post(self):
-        # ask for post name
-        post_text = input("Search by post text: ")
-        user = super().find_user(post_text)
+    def search_post(self,post=None):
+        if not post:
+            # ask for post name
+            post_text = input("Search by post text: ")
+            post = super().find_post(post_text)
+            print(post)
+
+        def follow_user(_post):
+            return self.user.follow(_post.user)
+
+        def like_post(_post):
+            _post.like_post(self.user)
+            return _post.display_likes()
+
+        def comment_post(_post):
+            comment = input("Type here: ")
+            _post.comment_post(self.user, comment)
+            return _post.display_comments()
+
+        def save_post(_post):
+            return self.user.save_post(_post)
+
+        post_events = {
+            "follow_user": follow_user,
+            "like": like_post,
+            "comment": comment_post,
+            "save": save_post,
+            "menu": self.main_events,
+
+        }
+        print(post_events[super().options_post(post_events)](post))
+        return self.search_post(post)
 
     def main_options(self):
         print(self.main_events[super().options(self.main_events)]())
         return self.main_options()
+
 
 dm = SocialMedia()
