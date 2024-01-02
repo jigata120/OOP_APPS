8bbea58358659503bff08b9b47fb8355e502a10f
Mihael Jigov <pplesho@gmail.com>
2024-01-02 16:47:51 +0200

    events for pots fixed and for user created

--- project/Main_App/Social_media.py
+++ project/Main_App/Social_media.py
@@ -5,7 +5,8 @@ class SocialMedia(SocialMediaFunctionalities):
 
     def __init__(self):
         super().__init__()
-        self.user = user1  # super().login()
+        self.user = user1
+        # super().login()
 
         self.main_events = {
             "content": self.content,
@@ -32,20 +33,52 @@ class SocialMedia(SocialMediaFunctionalities):
         content = super().personal_content(self.user)
         return content
 
-    def search_name(self):
-        # ask for username
-        username = input("Search by username: ")
-        user = super().find_user(username)
-        return user
+    def search_name(self, user=None):
 
-    def search_post(self,post=None):
+        if not user:
+            # ask for post name
+            username = input("Search by username: ")
+            user = super().find_user(username)
+            print(user.profile_view())
+
+        def follow_user(_user):
+            return self.user.follow(_user)
+
+        def save_post(_user):
+            return self.user.save_post(_user)
+
+        def main_options_redirect(_user):
+            return self.main_options()
+
+        def user_info(_user):
+            return str(_user)
+
+        def followers_list_view(_user):
+            return _user.followers_view()
+
+        def posts_list_view(_user):
+            return _user.posts_view()
+        def save_post_view(_user):
+            return _user.saves_view()
+        user_events = {
+            "follow_user": follow_user,
+            "info": user_info,
+            "followers_list": followers_list_view,
+            "posts_list": posts_list_view,
+            "saves_list": save_post_view,
+            "menu": main_options_redirect,
+        }
+        print(user_events[super().options(user_events)](user))
+        return self.search_name(user)
+
+    def search_post(self, post=None):
         if not post:
             # ask for post name
             post_text = input("Search by post text: ")
             post = super().find_post(post_text)
             print(post)
 
-        def follow_user(_post):
+        def follow_user_by_post(_post):
             return self.user.follow(_post.user)
 
         def like_post(_post):
@@ -60,15 +93,17 @@ class SocialMedia(SocialMediaFunctionalities):
         def save_post(_post):
             return self.user.save_post(_post)
 
+        def main_options_redirect(_post):
+            return self.main_options()
+
         post_events = {
-            "follow_user": follow_user,
+            "follow_user": follow_user_by_post,
             "like": like_post,
             "comment": comment_post,
             "save": save_post,
-            "menu": self.main_events,
-
+            "menu": main_options_redirect,
         }
-        print(post_events[super().options_post(post_events)](post))
+        print(post_events[super().options(post_events)](post))
         return self.search_post(post)
 
     def main_options(self):
